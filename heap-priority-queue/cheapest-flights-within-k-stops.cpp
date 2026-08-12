ac244a0f99f402863e834e9210e36c9af3709456
satyadas22006 <satyadas22006@gmail.com>
2026-08-12 12:25:17 +0530

    LC-cheapest-flights-within-k-stops [Runtime: N/A; Memory: N/A]

---
+++ heap-priority-queue/cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <map>
+#include <math.h>
+using namespace std;
+
+
+class Solution 
+{
+    public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
+    {
+        int tcost=0;
+        int stops=0;
+        //cost dest stops
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+        //pq.push({0,src});
+    //    vector<int> vis(n,0);
+
+        for(auto e:flights)
+        {
+            if(e[0]!=src)
+            {
+                continue;
+            }
+            pq.push({e[2],{e[1],1}}); //added cost and destination
+        }
+        while(!pq.empty())
+        {
+            auto [cost,info]=pq.top();
+            pq.pop();
+            int stops=info.second;
+            int dest=info.first;
+            if(stops==k+1)
+            {
+                if(dest==dst)
+                {
+                    return cost;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            for(auto x:flights)
+            {
+                if(x[0]==dest)
+                {
+                    int temp=cost+x[2];
+                    pq.push({temp,{x[1],stops+1}});
+                }   
+            }
+        }
+        return -1;
+    }
+};
