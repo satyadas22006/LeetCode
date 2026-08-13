63694466e023eb1d1c6374ddb0cde7202f1e2cf7
satyadas22006 <satyadas22006@gmail.com>
2026-08-13 11:29:40 +0530

    LC-greatest-common-divisor-traversal [Runtime: N/A; Memory: N/A]

--- union-find/greatest-common-divisor-traversal.cpp
+++ union-find/greatest-common-divisor-traversal.cpp
@@ -12,91 +12,107 @@
 #include <math.h>
 using namespace std;
 
-
 class Solution 
 {
-    public:
-        vector<int> parent;
-        vector<int> rank;
-        int find(int x)
-        {
-            if(parent[x]==x)
-            {
-                return x;
-            }
-            return parent[x]=find(parent[x]);
-        }
-        bool unite(int u,int v)
-        {
-            int pu=find(u);
-            int pv=find(v);
+public:
+    vector<int> parent;
+    vector<int> rank;
 
-            if(pu==pv)
+    int find(int x)
+    {
+        if(parent[x] == x)
+        {
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    bool unite(int u,int v)
+    {
+        int pu=find(u);
+        int pv=find(v);
+
+        if(pu==pv)
+        {
+            return false;
+        }
+
+        if(rank[pu]<rank[pv])
+        {
+            parent[pu]=pv;
+        }
+        else if(rank[pu]>rank[pv])
+        {
+            parent[pv]=pu;
+        }
+        else
+        {
+            parent[pu]=pv;
+            rank[pv]++;
+        }
+
+        return true;
+    }
+
+    int gcd(int a,int b)
+    {
+        while(b)
+        {
+            int temp=a%b;
+            a=b;
+            b=temp;
+        }
+
+        return a;
+    }
+
+    bool canTraverseAllPairs(vector<int>& nums) 
+    {
+        int n=nums.size();
+
+        if(n==1)
+        {
+            return true;
+        }
+
+        for(int x:nums)
+        {
+            if(x==1)
             {
-                //already same parent exists
                 return false;
             }
-
-            if(rank[pu]<rank[pv])
-            {
-                parent[pu]=pv;
-            }
-            else if(rank[pu]>rank[pv])
-            {
-                parent[pv]=pu;
-            }
-            else
-            {
-                parent[pu]=pv;
-                rank[pv]++;
-            }
-
-            return true;
-
         }
-        int gcd(int a,int b)
+
+        parent.resize(n);
+        rank.assign(n,0);
+
+        for(int i=0;i<n;i++)
         {
-            int ans=1;
-            for(int i=1;i<=min(a,b);i++)
-            {
-                if(a%i==0 && b%i==0)
-                {
-                    ans=i;
-                }
-            }
-            return ans;
+            parent[i]=i;
         }
-        bool canTraverseAllPairs(vector<int>& nums) 
+
+        for(int i=0;i<n;i++)
         {
-            //build dsu
-            int n=nums.size();
-            parent.resize(n);
-            rank.assign(n,0);
-            for(int i=0;i<n;i++)
+            for(int j=i+1;j<n;j++)
             {
-                parent[i]=i; //parents find krdiya sabka
-            }
-
-            for(int i=0;i<n;i++)
-            {
-                for(int j=i+1;j<n;j++)
+                if(gcd(nums[i],nums[j]) > 1)
                 {
-                    if(gcd(nums[i],nums[j]) > 1)
-                    {
-                        unite(i,j);
-                    }
+                    unite(i,j);
                 }
             }
-
-            int p=find(0);
-
-            for(int i=1;i<n;i++)
-            {
-                if(find(i)!=p)
-                {
-                    return false;
-                }
-            }
-            return true;
         }
+
+        int p=find(0);
+
+        for(int i=1;i<n;i++)
+        {
+            if(find(i)!=p)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 };
