a9202e9995caaa357f87a4a6abe15f1c7d18df5f
satyadas22006 <satyadas22006@gmail.com>
2026-08-18 20:23:13 +0530

    LC-coin-change-ii [Runtime: 26 ms; Memory: 36.6 MB]

--- array/coin-change-ii.cpp
+++ array/coin-change-ii.cpp
@@ -1,19 +1,50 @@
-class Solution {
-public:
-    int change(int amount, vector<int>& coins) 
-    {
-        vector<long long> dp(amount + 1, 0);
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <map>
+#include <math.h>
+using namespace std;
 
-        dp[0] = 1;
-
-        for(int ind = 0; ind < coins.size(); ind++)
+class Solution 
+{
+    public:
+        int f(int ind,int amount, vector<vector<int>>& dp,vector<int>& coins)
         {
-            for(int a = coins[ind]; a <= amount; a++)
+            if(ind==0)
             {
-                dp[a] += dp[a - coins[ind]];
+                if(amount%coins[0]==0)
+                {
+                    return 1;
+                }
+                return 0;
             }
-        }
+            if(dp[ind][amount]!=-1)
+            {
+                return dp[ind][amount];
+            }
+            //2 ways take, keeping take till u cant, else dont take
+            int nottake=f(ind-1,amount,dp,coins);
+            int take=0;
 
-        return dp[amount];
-    }
+            if(coins[ind]<=amount)
+            {
+                take=f(ind,amount-coins[ind],dp,coins);
+            }
+
+            return dp[ind][amount]=take+nottake;
+        }
+    public:
+        int change(int amount, vector<int>& coins) 
+        {
+            int n=coins.size()-1;
+            vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+            return f(n,amount,dp,coins);       
+        }
 };
