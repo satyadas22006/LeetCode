0af78886d35ba8e0f5105f6cbae6e244e7a06d63
satyadas22006 <satyadas22006@gmail.com>
2026-08-20 19:33:38 +0530

    LC-unique-paths-ii [Runtime: N/A; Memory: N/A]

--- dynamic-programming/unique-paths-ii.cpp
+++ dynamic-programming/unique-paths-ii.cpp
@@ -1,33 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <map>
+#include <math.h>
+using namespace std;
 class Solution 
 {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) 
     {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
+        //1d array solution
 
-        vector<int> dp(n, 0);
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
 
-        if(obstacleGrid[0][0] == 1)
-            return 0;
-
-        dp[0] = 1;
-
-        for(int r = 0; r < m; r++)
+        if(obstacleGrid[0][0]==1)
         {
-            for(int c = 0; c < n; c++)
+            return 0;
+        }
+
+        vector<int> dp(n,0); //stores the row
+
+        dp[0]=1;
+
+        for(int r=0;r<m;r++)
+        {
+            for(int c=0;c<n;c++)
             {
-                if(obstacleGrid[r][c] == 1)
+                if(obstacleGrid[r][c]==1)
                 {
-                    dp[c] = 0;
+                    dp[c]=0;
                 }
-                else if(c > 0)
+                else if(c>0)
                 {
-                    dp[c] = dp[c] + dp[c-1];
+                    dp[c]=dp[c] + dp[c-1];
                 }
             }
         }
-
         return dp[n-1];
     }
 };
