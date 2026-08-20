f467f2c0977c0793e83cf9c14f3719a5c67828f4
satyadas22006 <satyadas22006@gmail.com>
2026-08-20 19:26:29 +0530

    LC-unique-paths-ii [Runtime: 0 ms; Memory: 11.4 MB]

--- array/unique-paths-ii.cpp
+++ array/unique-paths-ii.cpp
@@ -1,16 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <array>
-#include <string>
-#include <unordered_map>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <unordered_set>
-#include <queue>
-#include <map>
-#include <math.h>
-using namespace std;
 class Solution 
 {
 public:
@@ -19,42 +6,28 @@ public:
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
 
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<int> dp(n, 0);
 
-        // Starting cell
         if(obstacleGrid[0][0] == 1)
             return 0;
 
-        dp[0][0] = 1;
+        dp[0] = 1;
 
         for(int r = 0; r < m; r++)
         {
             for(int c = 0; c < n; c++)
             {
-                // Skip starting cell
-                if(r == 0 && c == 0)
-                    continue;
-
-                // Obstacle
                 if(obstacleGrid[r][c] == 1)
                 {
-                    dp[r][c] = 0;
-                    continue;
+                    dp[c] = 0;
                 }
-
-                int up = 0;
-                int left = 0;
-
-                if(r > 0)
-                    up = dp[r-1][c];
-
-                if(c > 0)
-                    left = dp[r][c-1];
-
-                dp[r][c] = up + left;
+                else if(c > 0)
+                {
+                    dp[c] = dp[c] + dp[c-1];
+                }
             }
         }
 
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 };
