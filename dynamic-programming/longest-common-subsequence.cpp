a3b5b2232d1f3f82bda103fcb1ae568de3d07ba9
satyadas22006 <satyadas22006@gmail.com>
2026-08-20 21:00:13 +0530

    LC-longest-common-subsequence [Runtime: N/A; Memory: N/A]

--- dynamic-programming/longest-common-subsequence.cpp
+++ dynamic-programming/longest-common-subsequence.cpp
@@ -19,7 +19,7 @@ public:
         int m = text1.size();
         int n = text2.size();
 
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        vector<int> dp(m + 1,0);
         //shifted 1 index
         for(int i=1;i<=m;i++)
         {
@@ -28,14 +28,14 @@ public:
                 if(text1[i-1]==text2[j-1])
                 {
                     //check the -1 index as we shifted earlier
-                    dp[i][j]=1+dp[i-1][j-1];
+                    dp[j]=1+dp[j-1];
                 }
                 else
                 {
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    dp[j]=max(dp[j],dp[j-1]);
                 }
             }
         }
-        return dp[m][n];
+        return dp[n];
     }
 };
