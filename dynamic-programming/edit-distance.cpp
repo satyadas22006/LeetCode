659c966920d7d4b9827218a21e6415d99e234481
satyadas22006 <satyadas22006@gmail.com>
2026-08-21 20:31:29 +0530

    LC-edit-distance [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/edit-distance.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <map>
+#include <math.h>
+using namespace std;
+
+class Solution 
+{
+    public:
+        int f(int i,int j,string a,string b,vector<vector<int>> &dp)
+        {
+            if(i<0)
+            {
+                return j+1;
+            }
+            if(j<0)
+            {
+                return i+1;
+            }
+            if(dp[i][j]!=-1)
+            {
+                return dp[i][j];
+            }
+            //returns the minimum operations for a to go to target
+            if(a[i]==b[j])
+            {
+                //equal characters 
+                return f(i-1,j-1,a,b,dp);
+            }
+            //delete from a and thus a size is reduced i to i-1;
+            //insert to a that means sure equal i+1-1,j-1
+            //replaced(done)now go to next
+            return dp[i][j]=1+min(f(i-1,j,a,b,dp),min(f(i,j-1,a,b,dp),f(i-1,j-1,a,b,dp)));
+        }
+        int minDistance(string word1, string word2) 
+        {
+            vector<vector<int>> dp(word1.size(),vector<int>(word2.size(),-1));
+            return f(word1.size()-1,word2.size()-1,word1,word2,dp);
+        }
+};
