bfe7f9040cfb143512b9af7d6595b6815777f20e
satyadas22006 <satyadas22006@gmail.com>
2026-08-22 12:54:53 +0530

    LC-edit-distance [Runtime: N/A; Memory: N/A]

--- string/edit-distance.cpp
+++ string/edit-distance.cpp
@@ -29,7 +29,33 @@ class Solution
         }
         int minDistance(string word1, string word2) 
         {
-            vector<vector<int>> dp(word1.size(),vector<int>(word2.size(),-1));
-            return f(word1.size()-1,word2.size()-1,word1,word2,dp);
+            vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size()+1,-1));
+            for(int i=0;i<=word1.size();i++)
+            {
+                //0 represents -1
+                dp[i][0]=i+1;
+            }
+            for(int j=0;j<=word2.size();j++)
+            {
+                dp[0][j]=j+1;
+            }
+            //1-1 means starting position
+            for(int i=1;i<=word1.size();i++)
+            {
+                for(int j=1;j<=word2.size();j++)
+                {
+                    if(word1[i]==word2[j])
+                    {
+                        //if equal then move to next
+                        dp[i][j]=dp[i-1][j-1];
+                    }
+                    else
+                    {
+                        //not equal do some operation
+                        dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+                    }
+                }
+            }
+            return dp[word1.size()-1][word2.size()-1];
         }
 };
