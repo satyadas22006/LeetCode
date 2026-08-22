00ce630842fc53f984959baf2726593e0795afed
satyadas22006 <satyadas22006@gmail.com>
2026-08-22 14:02:05 +0530

    LC-best-time-to-buy-and-sell-stock-with-cooldown [Runtime: N/A; Memory: N/A]

--- dynamic-programming/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ dynamic-programming/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,14 +1,38 @@
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-    int sold = 0, hold = INT_MIN, rest = 0;
-    for (int i=0; i<prices.size(); ++i)
-    {
-        int prvSold = sold;
-        sold = hold + prices[i];
-        hold = max(hold, rest-prices[i]);
-        rest = max(rest, prvSold);
-    }
-    return max(sold, rest);
-}
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <map>
+#include <math.h>
+using namespace std;
+
+class Solution 
+{
+    public:
+        int maxProfit(vector<int>& prices) 
+        {
+            int n=prices.size();
+            vector<int> s0(n,-1);
+            vector<int> s1(n,-1);
+            vector<int> s2(n,-1);
+            //take rest only
+            s0[0]=0;
+            s1[0]=-prices[0];
+            s2[0]=INT_MIN;
+
+            for(int i=1;i<prices.size();i++)
+            {
+                s0[i]=max(s0[i-1],s2[i-1]);
+                s1[i]=max(s1[i-1],s0[i-1]-prices[i]);
+                s2[i]=s1[i-1]+prices[i];
+            }
+
+            return max(s0[n-1],s2[n-1]);
+        }
 };
