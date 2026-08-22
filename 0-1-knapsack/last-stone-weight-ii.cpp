2349fa59c46c57e03f6dee013d63c0cc8a93059c
satyadas22006 <satyadas22006@gmail.com>
2026-08-22 15:58:59 +0530

    LC-last-stone-weight-ii [Runtime: N/A; Memory: N/A]

--- 0-1-knapsack/last-stone-weight-ii.cpp
+++ 0-1-knapsack/last-stone-weight-ii.cpp
@@ -14,29 +14,29 @@ using namespace std;
 class Solution 
 {
     public:
-        int f(int target,int i,vector<int>& stones,vector<vector<int>>& dp)
-        {
-            if(i>=stones.size())
-            {
-                return 0;
-            }
-            if(target==0)
-            {
-                return 0;
-            }   
-            //2 options take or not_take
-            if(dp[i][target]!=-1)
-            {
-                return dp[i][target];
-            }
-            int take=INT_MIN;
-            if(stones[i]<=target)
-            {
-                take=stones[i]+f(target-stones[i],i+1,stones,dp);
-            }
-            int not_take=f(target,i+1,stones,dp);
-            return dp[i][target]=max(take,not_take);
-        }
+        // int f(int target,int i,vector<int>& stones,vector<vector<int>>& dp)
+        // {
+        //     if(i>=stones.size())
+        //     {
+        //         return 0;
+        //     }
+        //     if(target==0)
+        //     {
+        //         return 0;
+        //     }   
+        //     //2 options take or not_take
+        //     if(dp[i][target]!=-1)
+        //     {
+        //         return dp[i][target];
+        //     }
+        //     int take=INT_MIN;
+        //     if(stones[i]<=target)
+        //     {
+        //         take=stones[i]+f(target-stones[i],i+1,stones,dp);
+        //     }
+        //     int not_take=f(target,i+1,stones,dp);
+        //     return dp[i][target]=max(take,not_take);
+        //}
         int lastStoneWeightII(vector<int>& stones) 
         {
             //sort(stones.begin(),stones.end());
@@ -47,8 +47,30 @@ class Solution
             }
             int n=stones.size();
             int target=s/2;
-            vector<vector<int>> dp(n,vector<int>(target+1,-1));
-            
-            return s-2*f(target,0,stones,dp);
+            vector<vector<int>> dp(n,vector<int>(target+1,0));
+            for(int i=0;i<n;i++)
+            {
+                //when target is 0, return 0
+                dp[i][0]=0;
+            }
+            for(int j=stones[0];j<=target;j++)
+            {
+                dp[0][j]=stones[0];
+            }
+            for(int i=1;i<n;i++)
+            {
+                for(int j=0;j<=target;j++)
+                {
+                    int not_take=dp[i-1][j];
+                    int take=INT_MIN;
+                    if(stones[i]<=j)
+                    {
+                        take=stones[i] + dp[i-1][j-stones[i]];
+                    }
+                    dp[i][j]=max(take,not_take);
+                }
+            }
+            return s-2*dp[n-1][target];
+            //return s-2*f(target,0,stones,dp);
         }
 };
