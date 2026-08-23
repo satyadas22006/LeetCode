c5865f92a9f22ed87ae577686e7708cd5bf02eb0
satyadas22006 <satyadas22006@gmail.com>
2026-08-23 17:15:30 +0530

    LC-interleaving-string [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/interleaving-string.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <map>
+#include <math.h>
+using namespace std;
+
+
+class Solution 
+{
+    public:
+        int f(int i,int j,int k,string& s1,string& s2,string& s3,vector<vector<int>>& dp)
+        {
+            //base case
+            if(k>=s3.size())
+            {
+                return 1;
+            }
+            if(i>=s1.size() && j>=s2.size())
+            {
+                return 1;
+            }
+            if(dp[i][j]!=-1)
+            {
+                return dp[i][j];
+            }
+            int take1=0;
+            int take2=0;
+            if(i<s1.size() && s1[i]==s3[k])
+            {
+                take1=f(i+1,j,k+1,s1,s2,s3,dp);
+            }
+            if(j<s2.size() && s2[j]==s3[k])
+            {
+                take2=f(i,j+1,k+1,s1,s2,s3,dp);
+            }
+            return dp[i][j]=take1 || take2;
+        }
+        bool isInterleave(string s1, string s2, string s3) 
+        {
+            int x=s1.size();
+            int y=s2.size();
+            vector<vector<int>> dp(x+1,vector<int>(y+1,-1));
+            return (bool)f(0,0,0,s1,s2,s3,dp);
+        }
+};
