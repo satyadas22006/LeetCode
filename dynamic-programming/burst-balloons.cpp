bb4ba57f0b02fa9ce00c6688159004ef29d4cc0f
satyadas22006 <satyadas22006@gmail.com>
2026-08-23 19:41:00 +0530

    LC-burst-balloons [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/burst-balloons.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <map>
+#include <math.h>
+using namespace std;
+
+class Solution 
+{
+    public:
+        int f(int l,int r,vector<vector<int>> &dp,vector<int>& nums)
+        {
+            int k=l+(r-l)/2;
+            int ans=0;
+            for(int k=l;k<=r;k++)
+            {
+                int coins=nums[l-1]*nums[k]*nums[r+1]+f(l,k-1,dp,nums)+f(k+1,r,dp,nums);
+                ans=max(ans,coins);
+            }
+            return dp[l][r]=ans;
+        }
+        int maxCoins(vector<int>& nums) 
+        {
+            //2 things in a state
+            //total sum and index, max profit
+            nums.insert(nums.begin(),1);
+            nums.push_back(1);
+            int n=nums.size();
+            vector<vector<int>> dp(n,vector<int>(n,-1));
+
+            return f(1,n-2,dp,nums);
+        }
+};
