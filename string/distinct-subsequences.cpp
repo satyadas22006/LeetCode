b8c4db9f6ab7df5c9940aa1f8d7d463fa634a1bc
satyadas22006 <satyadas22006@gmail.com>
2026-08-24 08:43:28 +0530

    LC-distinct-subsequences [Runtime: N/A; Memory: N/A]

--- string/distinct-subsequences.cpp
+++ string/distinct-subsequences.cpp
@@ -1,42 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <map>
+#include <math.h>
+using namespace std;
+
+
 class Solution 
 {
-public:
-    int numDistinct(string s, string t) 
-    {
-        int m = s.size();
-        int n = t.size();
-
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-
-        // Empty t can always be formed in exactly 1 way
-        for(int i = 0; i <= m; i++)
+    public:
+        int numDistinct(string s, string t) 
         {
-            dp[i][n] = 1;
-        }
-
-        for(int i = m - 1; i >= 0; i--)
-        {
-            for(int j = n - 1; j >= 0; j--)
+            vector<vector<long long>> dp(s.size()+1,vector<long long>(t.size()+1,0));
+            for(int i=0;i<=s.size();i++)
             {
-                int not_take = dp[i + 1][j];
-                int take = 0;
+                dp[i][t.size()]=1;
+            }
+            for(int i=s.size()-1;i>=0;i--)
+            {
+                for(int j=t.size()-1;j>=0;j--)
+                {
+                    long long take=0;
+                    long long not_take=dp[i+1][j];
+                    if(s[i]==t[j])
+                    {
+                        take=dp[i+1][j+1];
+                    }
 
-                if(s[i] == t[j])
-                {
-                    take = dp[i + 1][j + 1];
-                }
-
-                if(take > INT_MAX - not_take)
-                {
-                    dp[i][j] = INT_MAX;
-                }
-                else
-                {
-                    dp[i][j] = take + not_take;
+                    if(take>INT_MAX-not_take)
+                    {
+                        dp[i][j]=INT_MAX;
+                    }
+                    else
+                    {
+                        dp[i][j]=take+not_take;
+                    }
                 }
             }
+            return dp[0][0];
         }
-
-        return dp[0][0];
-    }
 };
