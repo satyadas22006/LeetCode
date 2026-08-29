c1b4c6ed6196e7f2bdae7f24779fb60b90a224f9
satyadas22006 <satyadas22006@gmail.com>
2026-08-30 02:45:35 +0530

    LC-jump-game-vii [Runtime: N/A; Memory: N/A]

--- dynamic-programming/jump-game-vii.cpp
+++ dynamic-programming/jump-game-vii.cpp
@@ -1,37 +1,38 @@
-#include <iostream>
-#include <vector>
-#include <array>
-#include <string>
-#include <unordered_map>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <map>
-using namespace std;
-class Solution 
-{
-    public:
-        bool canReach(string s, int minJump, int maxJump) 
-        {
-            int n=s.size();
-            if(s[n-1]!='0' || s[0]!='0')
-            {
-                return false;
-            }
-            vector<int>dp(n,0);
-            dp[0]=1;
-            int cnt=0;
-            for(int i=1;i<n;i++)
-            {
-                int add=i-minJump;
-                if(add>=0 && dp[add]) cnt++;
-                int remove=i-maxJump-1;
-                if(remove>=0 && dp[remove]) cnt--;
-                if(s[i]=='0' && cnt>0)
-                {
-                    dp[i]=1;
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.size();
+
+        int l = 0;
+        int r = 0;
+
+        while (l <= r) {
+
+            int newL = r + 1;
+            int newR = r;
+
+            for (int i = l; i <= r; i++) {
+
+                if (s[i] == '0') {
+                    newL = min(newL, i + minJump);
+                    newR = max(newR, i + maxJump);
                 }
             }
-            return dp[n-1];
+
+            // Keep only valid indices
+            newL = max(newL, 0);
+            newR = min(newR, n - 1);
+
+            if (newL > newR)
+                return false;
+
+            if (newR == n - 1)
+                return true;
+
+            l = newL;
+            r = newR;
         }
+
+        return false;
+    }
 };
