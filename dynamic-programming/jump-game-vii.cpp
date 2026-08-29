ea80d32de33aa66ce65ddba3c988e276357083bc
satyadas22006 <satyadas22006@gmail.com>
2026-08-30 02:43:07 +0530

    LC-jump-game-vii [Runtime: N/A; Memory: N/A]

--- dynamic-programming/jump-game-vii.cpp
+++ dynamic-programming/jump-game-vii.cpp
@@ -11,41 +11,27 @@ using namespace std;
 class Solution 
 {
     public:
-        int f(int start,int &minJump,int &maxJump,string &s,vector<int>& dp)
-        {
-            if(start>=s.size()-1)
-            {
-                return true;
-            }
-            if(dp[start]!=-1)
-            {
-                return dp[start];
-            }
-            int l=start+minJump;
-            int r=min((int)s.size()-1,start+maxJump);
-            //bool check=false;
-            for(int i=l;i<=r;i++)
-            {
-                if(s[i]=='0')
-                {
-                    //check=true;
-                    int res=f(i,minJump,maxJump,s,dp);
-                    if(res)
-                    {
-                        return dp[start]=1;
-                    }
-                }
-            }
-            return dp[start]=0;
-        }
         bool canReach(string s, int minJump, int maxJump) 
         {
-            vector<int> dp(s.size(),-1);
-            if(s[0]!='0' || s.back()!='0')
+            int n=s.size();
+            if(s[n-1]!='0' || s[0]!='0')
             {
                 return false;
             }
-            //starts with 0 fs
-            return (bool)f(0,minJump,maxJump,s,dp);
+            vector<int>dp(n,0);
+            dp[0]=1;
+            int cnt=0;
+            for(int i=1;i<n;i++)
+            {
+                int add=i-minJump;
+                if(add>=0 && dp[add]) cnt++;
+                int remove=i-maxJump-1;
+                if(remove>=0 && dp[remove]) cnt--;
+                if(s[i]=='0' && cnt>0)
+                {
+                    dp[i]=1;
+                }
+            }
+            return dp[n-1];
         }
 };
