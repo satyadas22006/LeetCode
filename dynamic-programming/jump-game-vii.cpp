6543d6bcc74ef007d3ae448043446ad9198ca284
satyadas22006 <satyadas22006@gmail.com>
2026-08-30 03:13:36 +0530

    LC-jump-game-vii [Runtime: N/A; Memory: N/A]

--- dynamic-programming/jump-game-vii.cpp
+++ dynamic-programming/jump-game-vii.cpp
@@ -1,53 +1,42 @@
-#include <iostream>
-#include <vector>
-#include <array>
-#include <string>
-#include <unordered_map>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <map>
-using namespace std;
 class Solution 
 {
-    public:
-        bool canReach(string s, int minJump, int maxJump) 
+public:
+    bool canReach(string s, int minJump, int maxJump) 
+    {
+        int n = s.size();
+
+        if(s[0] != '0' || s[n-1] != '0')
         {
-            if(s[0]!='0' || s[s.size()-1]!='0')
-            {
-                return false;
-            }
-            queue<int> q;
-            q.push(0);
-            int r=INT_MIN;
-            while(!q.empty())
-            {
-                int i=q.front();
-                q.pop();
-                int nl=i+minJump;
-                int nr=i+maxJump;
-                if(nl>=(int)s.size())
-                {
-                    continue;
-                }
-                nr=min(nr,(int)s.size()-1);
-                if(nr<=r)
-                {
-                    continue;
-                }
-                for(int j=nl;j<=nr;j++)
-                {
-                    if(s[j]=='0')
-                    {
-                        if(j==s.size()-1)
-                        {
-                            return true;
-                        }
-                        q.push(j);
-                    }
-                }
-                r=max(r,nr);
-            }
             return false;
         }
+
+        int r = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(i > r || s[i] != '0')
+            {
+                continue;
+            }
+
+            int nl = i + minJump;
+            int nr = min(i + maxJump, n - 1);
+
+            if(nl > r + 1)
+                nl = r + 1;
+
+            for(int j = nl; j <= nr; j++)
+            {
+                if(s[j] == '0')
+                {
+                    if(j == n - 1)
+                        return true;
+
+                    r = max(r, j);
+                }
+            }
+        }
+
+        return n == 1;
+    }
 };
