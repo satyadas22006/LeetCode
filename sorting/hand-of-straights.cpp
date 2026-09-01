eca01963791b7ba7f19900d0a01b32fb27a4c592
satyadas22006 <satyadas22006@gmail.com>
2026-09-01 17:30:17 +0530

    LC-hand-of-straights [Runtime: N/A; Memory: N/A]

--- sorting/hand-of-straights.cpp
+++ sorting/hand-of-straights.cpp
@@ -1,38 +1,32 @@
-#include <iostream>
-#include <vector>
-#include <array>
-#include <string>
-#include <unordered_map>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <map>
-using namespace std;
-
 class Solution 
 {
-    public:
-        bool isNStraightHand(vector<int>& hand, int groupSize) 
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) 
+    {
+        if(hand.size() % groupSize != 0)
+            return false;
+
+        sort(hand.begin(), hand.end());
+
+        unordered_map<int,int> mp;
+
+        for(int x : hand)
+            mp[x]++;
+
+        for(int x : hand)
         {
-            if(hand.size()%groupSize!=0)
+            if(mp[x] == 0)
+                continue;
+
+            for(int j = 0; j < groupSize; j++)
             {
-                return false;
+                if(mp[x + j] == 0)
+                    return false;
+
+                mp[x + j]--;
             }
-            sort(hand.begin(),hand.end());
-            vector<unordered_map<int,int>> groups(groupSize);
-            for(int i:hand)
-            {
-                int k=0;
-                while(groups[k][i]>0 || groups[k].size()==groupSize)
-                {
-                    k++;
-                    if(k>=groupSize)
-                    {
-                        return false;
-                    }
-                }
-                groups[k][i]++;
-            }
-            return true;
         }
+
+        return true;
+    }
 };
