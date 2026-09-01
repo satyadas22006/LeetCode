429c1faec1f0f981ef75da8016d0e696e66579fb
satyadas22006 <satyadas22006@gmail.com>
2026-09-01 19:41:40 +0530

    LC-valid-parenthesis-string [Runtime: N/A; Memory: N/A]

--- stack/valid-parenthesis-string.cpp
+++ stack/valid-parenthesis-string.cpp
@@ -12,17 +12,17 @@ using namespace std;
 class Solution 
 {
     public:
-        bool f(int i,stack<char> store,string& s,vector<vector<int>>& dp)
+        bool f(int i,int store,string& s,vector<vector<int>>& dp)
         {
             if(i>=s.size())
             {
-                if(store.size()!=0)
+                if(store!=0)
                 {
                     return false;
                 }
                 return true;
             }
-            int sz=store.size();
+            int sz=store;
             if(dp[i][sz]!=-1)
             {
                 return dp[i][sz];
@@ -30,14 +30,14 @@ class Solution
             bool check=false;
             if(s[i]=='(')
             {
-                store.push(s[i]);
-                check=check || f(i+1,store,s,dp);
+                //store.push(s[i]);
+                check=check || f(i+1,store+1,s,dp);
             }
             else if(s[i]==')')
             {
-                if(store.size())
+                if(store)
                 {
-                    store.pop();
+                    store--;
                 }
                 else
                 {
@@ -49,15 +49,15 @@ class Solution
             {
                 //take 3 possibilities
                 //take it as (
-                store.push('(');
+                store++;
                 check=check || f(i+1,store,s,dp);
-                store.pop();
+                store--;
                 //take it as )
-                if(store.size())
+                if(store)
                 {
-                    store.pop();
+                    store--;
                     check=check || f(i+1,store,s,dp);
-                    store.push('(');
+                    store++;
                 }
                 //take it as nothing
                 check=check || f(i+1,store,s,dp);
@@ -66,8 +66,8 @@ class Solution
         }
         bool checkValidString(string s) 
         {
-            stack<char> store;
+            //stack<char> store;
             vector<vector<int>> dp(s.size()+1,vector<int>(s.size()+1,-1));
-            return f(0,store,s,dp);
+            return f(0,0,s,dp);
         }
 };
