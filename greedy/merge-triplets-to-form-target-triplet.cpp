22fb1cf389aea5494525bbdeecb3577e87ad5499
satyadas22006 <satyadas22006@gmail.com>
2026-09-01 18:31:55 +0530

    LC-merge-triplets-to-form-target-triplet [Runtime: N/A; Memory: N/A]

--- greedy/merge-triplets-to-form-target-triplet.cpp
+++ greedy/merge-triplets-to-form-target-triplet.cpp
@@ -21,8 +21,8 @@ class Solution
                 if(triplets[i][0]<=target[0] && triplets[i][1]<=target[1] && triplets[i][2]<=target[2])
                 {
                     if(triplets[i][0]==target[0]) flag1=true;
-                    if(triplets[i][1]<=target[1]) flag2=true;
-                    if(triplets[i][2]<=target[2]) flag3=true;
+                    if(triplets[i][1]==target[1]) flag2=true;
+                    if(triplets[i][2]==target[2]) flag3=true;
                 }
             }
             return (flag1&&flag2&&flag3);          
