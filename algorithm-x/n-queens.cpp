7e32ea57050ec0ceabd2b8229ae9983d02ecfe68
satyadas22006 <satyadas22006@gmail.com>
2026-09-02 14:13:28 +0530

    LC-n-queens [Runtime: N/A; Memory: N/A]

--- algorithm-x/n-queens.cpp
+++ algorithm-x/n-queens.cpp
@@ -1,22 +1,87 @@
-class Solution 
-{
-public:
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <map>
+using namespace std;
 
-    bool f(int i, int j, int queensleft,
-           vector<vector<int>> marked,
-           vector<vector<char>>& board,
-           vector<vector<string>>& ans)
+class Solution {
+public:
+    void f(int i,int j,int queensleft,vector<vector<int>> marked,vector<vector<char>>&board,vector<vector<string>>& ans)
     {
-        // all queens placed
-        if(queensleft == 0)
+        //you can only add somwhere if that place is not marked yet
+        //add i and j queen
+        //marking everyhing
+        if(queensleft==0)
         {
             vector<string> temp;
-
             for(int x = 0; x < board.size(); x++)
             {
                 string row = "";
-
                 for(int y = 0; y < board.size(); y++)
+                {
+                    row += board[x][y];
+                }
+                temp.push_back(row);
+            }
+            ans.push_back(temp);
+            return;
+        }
+        if(marked[i][j]==1) return;
+        for(int x=0;x<board.size();x++)
+            marked[x][j]=1;
+        for(int x=0;x<board.size();x++)
+            marked[i][x]=1;
+        marked[i][j]=1;
+        //now diagonals
+        int x=i+1;
+        int y=j+1;
+        while(x<board.size() && y<board.size())
+        {
+            marked[x][y]=1;
+            x++;
+            y++;
+        }
+        x=i-1;
+        y=j-1;
+        while(x>=0 && y>=0)
+        {
+            marked[x][y]=1;
+            x--;
+            y--;
+        }
+        // Down-left diagonal
+        x = i + 1;
+        y = j - 1;
+        while(x < board.size() && y >= 0)
+        {
+            marked[x][y] = 1;
+            x++;
+            y--;
+        }
+        // Up-right diagonal
+        x = i - 1;
+        y = j + 1;
+        while(x >= 0 && y < board.size())
+        {
+            marked[x][y] = 1;
+            x--;
+            y++;
+        }
+        board[i][j]='Q';
+        if(queensleft==1)
+        {
+            vector<string> temp;
+
+            for(int x=0;x<board.size();x++)
+            {
+                string row="";
+
+                for(int y=0;y<board.size();y++)
                 {
                     row += board[x][y];
                 }
@@ -26,121 +91,38 @@ public:
 
             ans.push_back(temp);
 
-            return true;
+            board[i][j]='.';
+            return;
         }
-
-        // add queen at i,j
-
-        // marking everything
-
-        // column
-        for(int x = 0; x < board.size(); x++)
+        //all are marked if and only if i and j has queen placed
+        for(int ni=0;ni<board.size();ni++)
         {
-            marked[x][j] = 1;
-        }
-
-        // row
-        for(int x = 0; x < board.size(); x++)
-        {
-            marked[i][x] = 1;
-        }
-
-        marked[i][j] = 1;
-
-        // -------------------------
-        // diagonals
-        // -------------------------
-
-        // ↘
-        int x = i + 1;
-        int y = j + 1;
-
-        while(x < board.size() && y < board.size())
-        {
-            marked[x][y] = 1;
-            x++;
-            y++;
-        }
-
-        // ↖
-        x = i - 1;
-        y = j - 1;
-
-        while(x >= 0 && y >= 0)
-        {
-            marked[x][y] = 1;
-            x--;
-            y--;
-        }
-
-        // ↙
-        x = i + 1;
-        y = j - 1;
-
-        while(x < board.size() && y >= 0)
-        {
-            marked[x][y] = 1;
-            x++;
-            y--;
-        }
-
-        // ↗
-        x = i - 1;
-        y = j + 1;
-
-        while(x >= 0 && y < board.size())
-        {
-            marked[x][y] = 1;
-            x--;
-            y++;
-        }
-
-        // actually put queen
-        board[i][j] = 'Q';
-
-        // now search for next queen
-        for(int ni = 0; ni < board.size(); ni++)
-        {
-            for(int nj = 0; nj < board.size(); nj++)
+            for(int nj=0;nj<board.size();nj++)
             {
-                if(marked[ni][nj] == 1)
+                if(marked[ni][nj]==0)
                 {
-                    continue;
+                    f(ni,nj,queensleft-1,marked,board,ans);
                 }
 
-                f(ni, nj, queensleft - 1,
-                  marked, board, ans);
             }
         }
-
-        // remove queen
-        board[i][j] = '.';
-
-        return true;
+        board[i][j]='.';
     }
-
     vector<vector<string>> solveNQueens(int n) 
     {
+        //dp state being index 2d matrix and queens left
+        //make the board
+        vector<vector<char>> board(n,vector<char>(n,'.'));
+        //currently empty
+        //making a marked 2d array
+        //0 unmarked and 1 marked
+        //int totalboxes=n*n;
+        vector<vector<int>> marked(n,vector<int>(n,0));
         vector<vector<string>> ans;
-
-        vector<vector<char>> board(
-            n,
-            vector<char>(n, '.')
-        );
-
-        vector<vector<int>> marked(
-            n,
-            vector<int>(n, 0)
-        );
-
-        for(int i = 0; i < n; i++)
+        for(int j=0;j<n;j++)
         {
-            for(int j = 0; j < n; j++)
-            {
-                f(i, j, n, marked, board, ans);
-            }
-        }
-
+            f(0,j,n,marked,board,ans);
+        }        
         return ans;
     }
 };
