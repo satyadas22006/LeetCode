a012697d86650d22beb1ffb96ca7654d68b3d795
satyadas22006 <satyadas22006@gmail.com>
2026-09-15 19:41:14 +0530

    LC-range-sum-query-2d-immutable [Runtime: N/A; Memory: N/A]

--- prefix-sum/range-sum-query-2d-immutable.cpp
+++ prefix-sum/range-sum-query-2d-immutable.cpp
@@ -12,19 +12,21 @@ public:
     vector<vector<int>> temp;
     NumMatrix(vector<vector<int>>& matrix) 
     {
-        temp=matrix;
-    }   
-    
-    int sumRegion(int row1, int col1, int row2, int col2) 
-    {
-        int ans=0;
-        for(int i=row1;i<=row2;i++)
+        if(matrix.size()==0 || matrix[0].size()==0) return;
+        int row=matrix.size();
+        int col=row>0 ? matrix[0].size() : 0;
+        temp=vector<vector<int>>(row+1,vector<int>(col+1,0));
+        for(int i=1;i<=row;i++)
         {
-            for(int j=col1;j<=col2;j++)
+            for(int j=1;j<=col;j++)
             {
-                ans+=temp[i][j];
+                temp[i][j]=matrix[i-1][j-1]+
+                        temp[i-1][j]+temp[i][j-1]-temp[i-1][j-1];
             }
         }
-        return ans;
+    }
+    int sumRegion(int row1, int col1, int row2, int col2) 
+    {
+        return temp[row2+1][col2+1]-temp[row2+1][col1]-temp[row1][col2+1]+temp[row1][col1];
     }
 };
