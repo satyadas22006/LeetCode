504e72ce9e2c7fba0adb77211c74df3bb96ce92f
satyadas22006 <satyadas22006@gmail.com>
2026-09-15 14:46:40 +0530

    LC-median-of-two-sorted-arrays [Runtime: N/A; Memory: N/A]

--- array/median-of-two-sorted-arrays.cpp
+++ array/median-of-two-sorted-arrays.cpp
@@ -1,30 +1,81 @@
-// Brute Force:
-               // 1.Merge Both Array
-              // 2.Sort them
-             // 3.Find Median
-            // TIME COMPLEXITY: O(n)+O(nlogn)+O(n)
-            // SPACE COMPLEXITY: O(1)
- 
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <map>
+using namespace std;
+
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-       // Initialization some neccessary variables
-        vector<int>v;
-        
-        // store the array in the new array
-        for(auto num:nums1)   // O(n1)
-            v.push_back(num);
-        
-        for(auto num:nums2)  // O(n2)
-            v.push_back(num);
-        
-        // Sort the array to find the median
-        sort(v.begin(),v.end());  // O(nlogn)
-        
-        // Find the median and Return it
-        int n=v.size();  // O(n)
-        
-        return n%2?v[n/2]:(v[n/2-1]+v[n/2])/2.0;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
+    {
+        int m=nums1.size();
+        int n=nums2.size();
+        int sum=m+n;
+        int i=0,j=0;
+        double ans=INT_MIN;
+        bool check=sum%2;
+        if(check) //false if even true if odd
+        {
+            sum=sum/2;
+        }
+        else
+        {
+            sum=sum/2;
+            sum++;
+        }
+        int count=0;
+        while(true)
+        {
+            if(nums1[i]>=nums2[j])
+            {
+                ans=nums1[i];
+                count++;
+                if(!check && count==sum) //even so 2 digits taking
+                {
+                    if(i!=m-1 && nums1[i]>=nums2[j])
+                    {
+                        ans+=nums1[i];
+                    }
+                    else
+                    {
+                        ans+=nums2[j];
+                    }
+                    return ans/2;
+                }
+                else if(check && count==sum)
+                {
+                    return ans;
+                }
+                if(i!=m-1) i++;
+            }
+            else if(nums1[i]<=nums2[j])
+            {
+                ans=nums2[j];
+                count++;
+                if(!check && count==sum)
+                {
+                    if(j!=n-1 && nums2[j]>=nums1[i])
+                    {
+                        ans+=nums2[j];
+                    }
+                    else
+                    {
+                        ans+=nums1[i];
+                    }
+                    return ans/2;
+                }
+                else if(check && count==sum)
+                {
+                    return ans;
+                }
+                if(j!=n-1) j++;
+            }
+        }
+        return -1;
     }
 };
-
